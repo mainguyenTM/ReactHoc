d0e51ee242c3ea3963b5c45a059eeb51dc2d9f1e
Mai Nguyen <mai.nguyen@vincere.io>
2019-10-30 22:31:52 +0700

    update todolist

--- todolist/src/component/TaskList.js
+++ todolist/src/component/TaskList.js
@@ -7,8 +7,10 @@ class TaskList extends Component {
         super(props)
         this.state = {
             tasks: ['Task 1', 'Task 2'],
-            showAddForm: false,
+            showAddForm: false
         }
+        this.handleFilter = this.handleFilter.bind(this);
+        this.filterText = '';
     }
     setStatus = () => {
         this.setState({
@@ -21,9 +23,30 @@ class TaskList extends Component {
             showEditForm: false
         })
     }
+    getTask = (name) => {
+        console.log('TASK LIST: '+this.state.tasks);
+        let temp = [];
+        if(name == '') return this.state.tasks;
+        this.state.tasks.filter(item => {
+            console.log('name:' +this.props.filterText +'---item:'+item);
+            if (item === this.props.filterText){
+                temp.push(item);
+            }
+        })
+        console.log('TASK LIST2: '+temp);
+        return temp;
+    }
     addTask = (name) => {
         this.state.tasks.push(name)
         this.forceUpdate()
+    }
+    handleFilter = (e) => {
+        var newTaskList = this.getTask();
+        console.log('New Task LIST2: '+newTaskList);
+        this.setState({
+            showAddForm: false,
+            tasks: newTaskList
+        })
     }
     render() {
         if (this.state.showAddForm === true) {
@@ -37,6 +60,7 @@ class TaskList extends Component {
                     <br />
                     <button type="button" className="btn btn-outline-primary" onClick={this.setStatus} >Add Task</button>
                     <h2>List Task</h2>
+                    <input value={this.props.filterText} onChange={this.handleFilter} className="filter" placeholder="Search a task..."></input>
                     <table className="table table-striped">
                         <thead>
                             <tr>
@@ -45,9 +69,9 @@ class TaskList extends Component {
                         </thead>
                         <tbody>
                             {
+                                //var renderData = this.state.filterTasks? this.state.filterTasks:this.state.tasks;
                                 this.state.tasks.map(function (name, index) {
-                                    return <TodoList name={name}
-                                    />
+                                    return <TodoList name={name}/>
                                 }.bind(this))
                             }
                         </tbody>
