344aed2a3229d365fbb70bd4ccd31acaf265e2ec
Mai Nguyen <mai.nguyen@vincere.io>
2019-10-30 19:40:06 +0700

    add todolist app

---
+++ todolist/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import TaskList from './component/TaskList';
+
+
+function App() {
+  return (
+    <div className="App">
+      <TaskList />
+    </div>
+  );
+}
+
+export default App;
